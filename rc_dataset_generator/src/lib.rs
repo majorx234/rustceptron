1bbce63afccf268d618db223f5a15b0237355516
majorx234 <majorx234@googlemail.com>
2023-08-30 00:26:20 +0200

    Add function to create cicles

--- rc_dataset_generator/src/lib.rs
+++ rc_dataset_generator/src/lib.rs
@@ -1,7 +1,7 @@
 use rc_data::Layer;
 
 #[inline(always)]
-fn clampi(x: usize, low: usize, high: usize) -> usize {
+fn clampi(x: isize, low: isize, high: isize) -> isize {
     if x < low {
         return low;
     };
@@ -11,16 +11,33 @@ fn clampi(x: usize, low: usize, high: usize) -> usize {
     x
 }
 
-fn layer_fill_rect(layer: &mut Layer<f32>, x: usize, y: usize, w: usize, h: usize, value: f32) {
+fn layer_fill_rect(layer: &mut Layer<f32>, x: isize, y: isize, w: isize, h: isize, value: f32) {
     assert!(w > 0);
     assert!(h > 0);
-    let x0 = clampi(x, 0, layer.width - 1);
-    let y0 = clampi(y, 0, layer.height - 1);
-    let x1 = clampi(x0 + w, 0, layer.width - 1);
-    let y1 = clampi(y0 + h, 0, layer.height - 1);
+    let x0 = clampi(x, 0, layer.width as isize - 1);
+    let y0 = clampi(y, 0, layer.height as isize - 1);
+    let x1 = clampi(x0 + w, 0, layer.width as isize - 1);
+    let y1 = clampi(y0 + h, 0, layer.height as isize - 1);
     for y in y0..y1 {
         for x in x0..x1 {
-            layer.data[y * (layer.width - 1) + x] = value;
+            layer.data[y as usize * (layer.width - 1) + x as usize] = value;
+        }
+    }
+}
+
+fn layer_fill_circle(layer: &mut Layer<f32>, cx: isize, cy: isize, r: isize, value: f32) {
+    assert!(r > 0);
+    let x0 = clampi(cx - r, 0, layer.width as isize - 1);
+    let y0 = clampi(cy - r, 0, layer.height as isize - 1);
+    let x1 = clampi(cx + r, 0, layer.width as isize - 1);
+    let y1 = clampi(cy + r, 0, layer.height as isize - 1);
+    for y in y0..y1 {
+        for x in x0..x1 {
+            let dx = x - cx;
+            let dy = y - cy;
+            if dx * dx + dy * dy <= r * r {
+                layer.data[y as usize * (layer.width - 1) + x as usize] = value;
+            }
         }
     }
 }
