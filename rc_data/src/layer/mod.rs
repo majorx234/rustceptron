b7e8634d1bca5f72a0b233dc4d77886fab9df02b
MajorX234 <majorx234@googlemail.com>
2024-11-18 00:36:37 +0100

    Add vector multiplication

--- rc_data/src/layer/mod.rs
+++ rc_data/src/layer/mod.rs
@@ -4,14 +4,14 @@ use activation_fct::ActivationFct;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct Layer<T: Float> {
+pub struct Layer<T: Float + std::ops::AddAssign> {
     pub width: usize,
     pub height: usize,
     pub data: Vec<T>,
     activation_fct: Box<dyn ActivationFct + 'static>,
 }
 
-impl<T: Float> Layer<T> {
+impl<T: Float + std::ops::AddAssign> Layer<T> {
     pub fn new(width: usize, height: usize, activation_fct: Box<dyn ActivationFct>) -> Self {
         Self {
             width,
@@ -27,9 +27,20 @@ impl<T: Float> Layer<T> {
         let last = self.data.len() - 1;
         self.data[last] = One::one();
     }
+    pub fn vector_mul(&mut self, in_vec: Vec<T>, out_vec: &mut Vec<T>) {
+        assert_eq!(in_vec.len(), self.width);
+        assert_eq!(out_vec.len(), self.height);
+
+        for i in 0..self.height {
+            out_vec[i] = T::zero();
+            for j in 0..self.width {
+                out_vec[i] += self.data[j + i * self.width];
+            }
+        }
+    }
 }
 
-impl<'a, T: Float> Layer<T> {
+impl<'a, T: Float + std::ops::AddAssign> Layer<T> {
     pub fn print(self) {
         for y in 0..self.height {
             for x in 0..self.width {
