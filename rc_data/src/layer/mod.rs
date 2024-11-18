950460c36d6299b17a462c44ac00f1217069bd74
MajorX234 <majorx234@googlemail.com>
2024-11-18 03:27:16 +0100

    Implemented feed forward , use now just f32

--- rc_data/src/layer/mod.rs
+++ rc_data/src/layer/mod.rs
@@ -1,50 +1,51 @@
-use num_traits::{Float, One, Zero};
 pub mod activation_fct;
 use activation_fct::ActivationFct;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct Layer<T: Float + std::ops::AddAssign> {
+pub struct Layer {
     pub width: usize,
     pub height: usize,
-    pub data: Vec<T>,
-    activation_fct: Box<dyn ActivationFct + 'static>,
+    pub data: Vec<f32>,
+    pub activation_fct: Box<dyn ActivationFct + 'static>,
 }
 
-impl<T: Float + std::ops::AddAssign> Layer<T> {
+impl Layer {
     pub fn new(width: usize, height: usize, activation_fct: Box<dyn ActivationFct>) -> Self {
         Self {
             width,
             height,
-            data: vec![Zero::zero(); width * height + 1],
+            data: vec![0.0; width * height + 1],
             activation_fct,
         }
     }
     pub fn clear(&mut self) {
         self.data.iter_mut().for_each(|x| {
-            *x = Zero::zero();
+            *x = 0.0;
         });
         let last = self.data.len() - 1;
-        self.data[last] = One::one();
+        self.data[last] = 1.0;
     }
-    pub fn vector_mul(&mut self, in_vec: Vec<T>, out_vec: &mut Vec<T>) {
-        assert_eq!(in_vec.len(), self.width);
-        assert_eq!(out_vec.len(), self.height);
+    pub fn vector_mul(&mut self, vec: &mut [Vec<f32>]) {
+        assert!(vec.len() == 2);
+        assert_eq!(vec[0].len(), self.width);
+        assert_eq!(vec[1].len(), self.height);
 
         for i in 0..self.height {
-            out_vec[i] = T::zero();
+            let mut sum = 0.0;
             for j in 0..self.width {
-                out_vec[i] += self.data[j + i * self.width];
+                sum += self.data[j + i * self.width] * vec[1][j];
             }
+            vec[1][i] = sum;
         }
     }
 }
 
-impl<'a, T: Float + std::ops::AddAssign> Layer<T> {
+impl<'a> Layer {
     pub fn print(self) {
         for y in 0..self.height {
             for x in 0..self.width {
-                if self.data[y * self.width + x] > Zero::zero() {
+                if self.data[y * self.width + x] > 0.0 {
                     print!("#");
                 }
             }
--- rc_data/src/network/mod.rs
+++ rc_data/src/network/mod.rs
@@ -1,26 +1,26 @@
-use crate::layer::{
-    activation_fct::{ActivationFct, ReLU},
-    Layer,
-};
-use num_traits::{Float, Inv, One, Zero};
-use rand::{distributions::Uniform, prelude::Distribution, thread_rng, Rng};
-pub struct Network<T: Float + std::ops::AddAssign> {
-    pub layers: Vec<Layer<T>>,
+use crate::layer::{activation_fct::ReLU, Layer};
+use rand::Rng;
+pub struct Network {
+    pub layers: Vec<Layer>,
+    pub bias: Vec<Vec<f32>>,
     pub step_count: u32,
 }
 
-impl<T: Float + rand::distributions::uniform::SampleUniform + std::ops::AddAssign> Network<T> {
+impl Network {
     pub fn new(layer_sizes: Vec<(usize, usize)>) -> Self {
         let mut layers = Vec::new();
+        let mut bias = Vec::new();
         for layer_size in layer_sizes {
-            layers.push(Layer::<T>::new(
+            layers.push(Layer::new(
                 layer_size.0,
                 layer_size.1,
                 Box::new(ReLU::new(1.0)),
             ));
+            bias.push(vec![0.0; layer_size.1]);
         }
         Network {
             layers,
+            bias,
             step_count: 0,
         }
     }
@@ -28,18 +28,42 @@ impl<T: Float + rand::distributions::uniform::SampleUniform + std::ops::AddAssig
         let mut rng = rand::thread_rng();
         for layer in self.layers.iter_mut() {
             layer.data.iter_mut().for_each(|x| {
-                let lower = Zero::zero();
-                let upper = One::one();
+                let lower = 0.0;
+                let upper = 1.0;
+                *x = rng.gen_range(lower..upper);
+            });
+        }
+        for bias_vec in self.bias.iter_mut() {
+            bias_vec.iter_mut().for_each(|x| {
+                let lower = 0.0;
+                let upper = 1.0;
                 *x = rng.gen_range(lower..upper);
             });
         }
     }
-    pub fn forward(&mut self, input: Vec<T>) -> Vec<T> {
+    pub fn forward(&mut self, input: Vec<f32>) {
+        let mut activation_vecs: Vec<Vec<f32>> = Vec::new();
+
+        activation_vecs.push(input);
         for i in 1..self.layers.len() {
-            let activation_vec = vec![0.0; self.layers[i].height];
-            // TODO: Vector layer Multiplication
-            // sum with biases per layer
-            // TODO: calc Activation function
+            let activation_fct: Vec<f32> = vec![0.0; self.layers[i].width];
+            activation_vecs.push(activation_fct);
+        }
+        self.forward_step(&mut activation_vecs);
+    }
+    fn forward_step(&mut self, activation_vec: &mut Vec<Vec<f32>>) -> Vec<f32> {
+        for i in 0..self.layers.len() {
+            // multiplicate layer with input
+            self.layers[i].vector_mul(&mut activation_vec[i..i + 1]);
+            // calc sum with bias vector
+            activation_vec[i]
+                .iter_mut()
+                .enumerate()
+                .for_each(|(idx, x)| *x += self.bias[i][idx]);
+            // calc activation function
+            activation_vec[i].iter_mut().for_each(|x| {
+                *x = self.layers[i].activation_fct.fvalue(*x);
+            });
         }
         Vec::new()
     }
